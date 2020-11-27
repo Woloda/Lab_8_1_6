27e7158642c5c0b0770a34369ddf7176a57b827f
Woloda <dorozovecvoloda@gmail.com>
2020-11-27 08:41:24 +0200

    Lab__8_1_6

---
+++ Lab_8.1(6)/Lab_8.1(6).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string lit_r = "ssqQSfqsSWfs";
+	cout << "Сфомований літерний рядок: " << endl << endl << lit_r << endl << endl;
+	string para = "SQ";
+	string para1 = "QS";
+
+	bool b = false;
+	if (lit_r.find(para) != -1)
+		b = true;
+	if (lit_r.find(para1) != -1)
+		b = true;
+	if (b)
+		cout << "Так є серед цих символів пара сусідніх букв “SQ” або ”QS”." << endl;
+	else
+		cout << "Ні не має серед цих символів пара сусідніх букв “SQ” або ”QS”." << endl;
+
+	return 0;
+}
---
+++ UnitTest(Lab_8.1(6))/UnitTest(Lab_8.1(6)).cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+using namespace std;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab816
+{
+	TEST_CLASS(UnitTestLab816)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+				string lit_r = "ssqQSfqsSWfs";
+				string para = "SQ";
+				string para1 = "QS";
+
+				bool b = false;
+				if (lit_r.find(para) != -1)
+					b = true;
+				if (lit_r.find(para1) != -1)
+					b = true;
+
+				Assert::AreEqual(b, true);
+		}
+	};
+}
